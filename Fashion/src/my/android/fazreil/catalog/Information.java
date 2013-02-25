a533394fcf9584b11b20ec8cd2557b7c785d1a61
fab <fab@fab-ThinkPad-X200>
2013-02-26 07:04:53 +0800

    done with cover

---
+++ Fashion/src/my/android/fazreil/catalog/Information.java
@@ -0,0 +1,22 @@
+package my.android.fazreil.catalog;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class Information extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_information);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_information, menu);
+		return true;
+	}
+
+}
