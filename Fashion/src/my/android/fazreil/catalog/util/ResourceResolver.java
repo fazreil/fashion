90dbee511a8b430f4471489e2bde8ab4df1af544
fab <fab@fab-ThinkPad-X200.(none)>
2013-06-14 04:49:41 +0800

    resource resolver

---
+++ Fashion/src/my/android/fazreil/catalog/util/ResourceResolver.java
@@ -0,0 +1,29 @@
+package my.android.fazreil.catalog.util;
+
+import android.graphics.drawable.BitmapDrawable;
+
+//http://stackoverflow.com/questions/9156698/how-to-get-images-dynamiclly-from-drawable-folder
+
+public class ResourceResolver {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	public ResourceResolver()
+	{}
+	
+	public static int[] resolveImages()
+	{
+		int[] returnImages;
+		BitmapDrawable bitmapDrawable;
+		
+		
+		return returnImages;
+	}
+
+}
